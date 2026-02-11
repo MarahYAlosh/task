e842dc62018130a6a9200fe0f57455af7e894f90
ASUS <marah.alosh987@gmail.com>
2026-02-11 12:28:25 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import {  HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import MessagesPage from './pages/MessagesPage'
 
 
@@ -8,12 +8,12 @@ function App() {
 
 
   return (
-  <BrowserRouter>
+  <HashRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/messages" />} />
         <Route path="/messages" element={<MessagesPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
